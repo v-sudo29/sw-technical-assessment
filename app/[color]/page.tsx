f5a2edd7f117afa4f429eb3f557207b01ad59372
v-sudo29 <117846985+v-sudo29@users.noreply.github.com>
2024-07-01 08:51:38 -0700

    Implement dimming, open/close, logic for modal 1

--- app/[color]/page.tsx
+++ app/[color]/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+import { useState } from 'react'
 import Modal from '@/components/Modal'
 
 const COLORS = {
@@ -7,6 +9,11 @@ const COLORS = {
 }
 
 const Page = ({ params }: { params: { color: string } }) => {
+  const [isModalOneOpen, setIsModalOneOpen] = useState(true)
+
+  const handleModalOneOpen = () => setIsModalOneOpen(true)
+  const handleModalOneClose = () => setIsModalOneOpen(false)
+
   const pageColor =
     params.color === COLORS.GREEN
       ? 'bg-[green]'
@@ -16,10 +23,55 @@ const Page = ({ params }: { params: { color: string } }) => {
       ? 'bg-[red]'
       : 'bg-[white]'
 
+  if (
+    params.color !== COLORS.GREEN &&
+    params.color !== COLORS.BLUE &&
+    params.color !== COLORS.RED
+  )
+    return <></>
+
   return (
     <main className={'min-h-[200vh]' + ' ' + pageColor}>
       <h1 className='text-white uppercase'>{params.color}</h1>
-      <Modal />
+      <div className='flex gap-2'>
+        {params.color === COLORS.GREEN && (
+          <>
+            <button
+              className='border'
+              onClick={handleModalOneOpen}
+            >
+              Open Modal 1
+            </button>
+            <button className='border'>Open Modal 2</button>
+          </>
+        )}
+        {params.color === COLORS.BLUE && (
+          <>
+            <button className='border'>Open Modal 2</button>
+            <button className='border'>Open Modal 3</button>
+          </>
+        )}
+        {params.color === COLORS.RED && (
+          <>
+            <button
+              className='border'
+              onClick={handleModalOneOpen}
+            >
+              Open Modal 1
+            </button>
+            <button className='border'>Open Modal 3</button>
+          </>
+        )}
+      </div>
+      {/* MODAL 1 */}
+      <Modal
+        open={isModalOneOpen}
+        handleClose={handleModalOneClose}
+        title='Modal 1'
+        subheading='Lorem Ipsum is simply'
+        date='Thursday, Jun 22nd, 2024'
+        time='06:00pm - 07:30pm EST'
+      />
     </main>
   )
 }
--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,56 +1,94 @@
+import { useEffect } from 'react'
 import MobileLogo from './MobileLogo'
 import TabletLogo from './TabletLogo'
 import CloseIcon from './icons/CloseIcon'
 
-const Modal = () => {
-  return (
-    <div className='rounded-xl max-w-[339px] overflow-hidden font-poppins sm:max-w-[640px]'>
-      {/* TOP SECTION */}
-      <div className='relative flex flex-col gap-3 bg-primary-purple px-6 pb-6 pt-12 text-white font-medium sm:p-12'>
-        <h1 className='text-[18px] leading-[25px] sm:text-[32px] sm:leading-[44.8px]'>
-          Modal 1
-        </h1>
-        <p className='text-sm'>Lorem Ipsum is simply</p>
-        <div className='text-sm sm:text-base'>
-          <p>Thursday, Jun 22, 2024</p>
-          <p>06:00pm - 07:30pm EST</p>
-        </div>
-        <div className='absolute right-0 bottom-0 sm:hidden'>
-          <MobileLogo />
-        </div>
-        <div className='absolute right-0 bottom-0 hidden sm:block'>
-          <TabletLogo />
-        </div>
-      </div>
+interface ModalProps {
+  open: boolean
+  handleClose: () => void
+  title: string
+  subheading: string
+  date: string
+  time: string
+}
 
-      {/* BOTTOM SECTION */}
-      <div className='bg-white'>
-        <div className='flex justify-between px-6 pt-6 pb-4'>
-          <div>
-            <p className='text-sm leading-[19.6px] text-secondary-black'>
-              Lorem Ipsum
-            </p>
-            <p className='leading-[22.4px] font-bold text-primary-purple'>
-              Lorem Ipsum
+const Modal = ({
+  open,
+  handleClose,
+  title,
+  subheading,
+  date,
+  time,
+}: ModalProps) => {
+  // Prevents scrolling when modal is open
+  useEffect(() => {
+    const body = document.querySelector('body') as HTMLBodyElement
+
+    if (open) body.style.overflow = 'hidden'
+    else body.style.overflow = 'visible'
+  }, [open])
+
+  if (!open) return <></>
+  return (
+    <div className='fixed top-0 w-full h-full flex justify-center z-10 border border-blue-500'>
+      {/* OVERLAY */}
+      <div
+        className='fixed bg-black opacity-50 w-full h-full'
+        onClick={handleClose}
+      ></div>
+
+      {/* MODAL */}
+      <div className='absolute top-10 rounded-xl max-w-[339px] overflow-hidden font-poppins sm:max-w-[640px]'>
+        {/* TOP SECTION */}
+        <div className='relative flex flex-col gap-3 bg-primary-purple px-6 pb-6 pt-12 text-white font-medium sm:p-12'>
+          <h1 className='text-[18px] leading-[25px] sm:text-[32px] sm:leading-[44.8px]'>
+            {title}
+          </h1>
+          <p className='text-sm'>{subheading}</p>
+          <div className='text-sm sm:text-base'>
+            <p>{date}</p>
+            <p>{time}</p>
+          </div>
+          <div className='absolute right-0 bottom-0 sm:hidden'>
+            <MobileLogo />
+          </div>
+          <div className='absolute right-0 bottom-0 hidden sm:block'>
+            <TabletLogo />
+          </div>
+        </div>
+
+        {/* BOTTOM SECTION */}
+        <div className='bg-white'>
+          <div className='flex justify-between px-6 pt-6 pb-4'>
+            <div>
+              <p className='text-sm leading-[19.6px] text-secondary-black'>
+                Lorem Ipsum
+              </p>
+              <p className='leading-[22.4px] font-bold text-primary-purple'>
+                Lorem Ipsum
+              </p>
+            </div>
+            <button
+              className='mb-auto'
+              onClick={handleClose}
+            >
+              <CloseIcon />
+            </button>
+          </div>
+          <div className='px-6 pb-6'>
+            <p className='font-medium leading-[22.4px] text-primary-black'>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry&apos;s
             </p>
           </div>
-          <button className='mb-auto'>
-            <CloseIcon />
-          </button>
-        </div>
-        <div className='px-6 pb-6'>
-          <p className='font-medium leading-[22.4px] text-primary-black'>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry&apos;s
-          </p>
-        </div>
-        <div className='border-t border-t-[#E5E7EB] p-6'>
-          <p className='font-medium text-sm text-center text-secondary-black leading-[19.6px] mb-[10px] sm:font-normal'>
-            Lorem Ipsum is simply dummy text
-          </p>
-          <button className='rounded-xl w-full bg-[#32A7AD1A] font-medium font-roboto text-[#292B2E33] py-4 sm:uppercase sm:font-semibold'>
-            Lorem Ipsum
-          </button>
+          <div className='border-t border-t-[#E5E7EB] p-6'>
+            <p className='font-medium text-sm text-center text-secondary-black leading-[19.6px] mb-[10px] sm:font-normal'>
+              Lorem Ipsum is simply dummy text
+            </p>
+            <button className='rounded-xl w-full bg-[#32A7AD1A] font-medium font-roboto text-[#292B2E33] py-4 sm:uppercase sm:font-semibold'>
+              Lorem Ipsum
+            </button>
+          </div>
         </div>
       </div>
     </div>
